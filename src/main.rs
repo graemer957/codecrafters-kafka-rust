882bbe8d2851427b90319c05b4d61c3706824a3d
Graeme Read <graeme@sigma957.net>
2024-11-06 09:06:03 +0000

    Send Correlation ID

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::net::TcpListener;
+use bytes::{BufMut, BytesMut};
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
 
+    let message_size: i32 = 0;
+    let correlation_id: i32 = 7;
+    let mut message = BytesMut::new();
+    message.put_i32(message_size);
+    message.put_i32(correlation_id);
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let _ = stream.write(&message[..]);
             }
             Err(e) => {
                 println!("error: {}", e);

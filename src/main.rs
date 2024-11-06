704cee2b11ea672a818d2f16d9558fc5c8e63975
Graeme Read <graeme@sigma957.net>
2024-11-06 09:44:32 +0000

    Parse Correlation ID

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 use bytes::{BufMut, BytesMut};
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-
-    let message_size: i32 = 0;
-    let correlation_id: i32 = 7;
-    let mut message = BytesMut::new();
-    message.put_i32(message_size);
-    message.put_i32(correlation_id);
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut request = [0; 12];
+                stream
+                    .read_exact(&mut request)
+                    .expect("12 bytes to be sent");
+
+                let message_size: i32 = 0;
+                let mut message = BytesMut::new();
+                message.put_i32(message_size);
+                message.put(&request[8..]);
+
                 let _ = stream.write(&message[..]);
             }
             Err(e) => {
